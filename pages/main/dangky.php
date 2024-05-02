383dbfd56d98c6e0239e74d05a34944036f8faf5
Tuan2 <thuongvinhquy11b9nh1@gmail.com>
2024-05-02 23:42:22 +0700

    fix: error connect string

--- pages/main/dangky.php
+++ pages/main/dangky.php
@@ -1,24 +1,28 @@
 <?php
-	if(isset($_POST['dangky'])) {
-		$tenkhachhang = $_POST['hovaten'];
-		$email = $_POST['email'];
-		$dienthoai = $_POST['dienthoai'];
-		$matkhau = md5($_POST['matkhau']);
-		$diachi = $_POST['diachi'];
-		$sql_dangky = mysqli_query($mysqli,"INSERT INTO tbl_dangky(tenkhachhang,email,diachi,matkhau,dienthoai) VALUE('".$tenkhachhang."','".$email."','".$diachi."','".$matkhau."','".$dienthoai."')");
-		if($sql_dangky){
-			echo '<p style="color:green">Bạn đã đăng ký thành công</p>';
-			$_SESSION['dangky'] = $tenkhachhang;
-			$_SESSION['id_khachhang'] = mysqli_insert_id($mysqli);
-			?>
-				<script>
-					window.alert("Đăng Ký Thành Công");
-				window.location="index.php?quanly=giohang";
-				</script>
-			<?php
-		}
-	}
+if(isset($_POST['dangky'])) {
+    $tenkhachhang = $_POST['hovaten'];
+    $email = $_POST['email'];
+    $dienthoai = $_POST['dienthoai'];
+    $matkhau = md5($_POST['matkhau']);
+    $diachi = $_POST['diachi'];
+    
+    // Thêm trường 'phanquyen' vào câu lệnh INSERT
+    $sql_dangky = mysqli_query($mysqli,"INSERT INTO tbl_dangky(tenkhachhang,email,diachi,matkhau,dienthoai, phanquyen) VALUES('".$tenkhachhang."','".$email."','".$diachi."','".$matkhau."','".$dienthoai."', 0)");
+    
+    if($sql_dangky){
+        echo '<p style="color:green">Bạn đã đăng ký thành công</p>';
+        $_SESSION['dangky'] = $tenkhachhang;
+        $_SESSION['id_khachhang'] = mysqli_insert_id($mysqli);
+        ?>
+        <script>
+            window.alert("Đăng Ký Thành Công");
+            window.location="index.php?quanly=giohang";
+        </script>
+        <?php
+    }
+}
 ?>
+
 <!------ Include the above in your HEAD tag ---------->
 <style>
        .form {
